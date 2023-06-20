e4259a63e8b1b34db42d26c9788e04f6ec644466
LostLuma <lilly@lostluma.net>
2023-06-20 14:47:10 +0200

    Prevent white flash loading pages while on dark theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { css, Global } from "@emotion/react";
+import { css } from "@emotion/react";
 import { StrictMode } from "react";
 import { HeadProvider, Link } from "react-head";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -6,15 +6,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home, NotFound } from "./pages";
 
 const BlobLuma = new URL("./media/blobluma.png", import.meta.url);
-
-const global = css({
-  "@media (prefers-color-scheme: dark)": {
-    "body": {
-      color: "#ffffff",
-      backgroundColor: "#030303",
-    },
-  },
-})
 
 const app = css({
   marginTop: "15%",
@@ -24,21 +15,18 @@ const app = css({
 
 export default function App() {
   return (
-    <>
-      <Global styles={global} />
-      <div className="app" css={app}>
-        <StrictMode>
-          <BrowserRouter>
-            <HeadProvider>
-              <Link rel="icon" href={BlobLuma.href} />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </HeadProvider>
-          </BrowserRouter>
-        </StrictMode>
-      </div>
-    </>
+    <div className="app" css={app}>
+      <StrictMode>
+        <BrowserRouter>
+          <HeadProvider>
+            <Link rel="icon" href={BlobLuma.href} />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </HeadProvider>
+        </BrowserRouter>
+      </StrictMode>
+    </div>
   );
 }
